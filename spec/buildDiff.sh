aa34d463b4514c1384fc2594f0f3268d13b1e2a2
Paliak <91493239+Paliak@users.noreply.github.com>
2024-07-01 19:50:10 +0200

    FIX: Minor security improvements.
    Quote variables.
    
    Instead of using a sub shell built in bash functionality is now used to strip non ascii characters from the build link generating a string that will be used as a filename.
    
    The dawidd6/action-download-artifact is known to allow for creation of vulnerable workflows.
    This workflow does not execute any code coming from artefacts. I've additionally added paths everywhere to harden against symlinks as suggested in the following link:
    https://www.legitsecurity.com/blog/github-actions-that-open-the-door-to-cicd-pipeline-attacks

--- spec/buildDiff.sh
+++ spec/buildDiff.sh
@@ -9,19 +9,19 @@ then
 fi
 
 # Copy mounted workdir to allow for changes during test run
-rm -rf /tmp/workdir && mkdir /tmp/workdir && cp -rf $WORKDIR/. /tmp/workdir/ && cd /tmp/workdir
+rm -rf /tmp/workdir && mkdir /tmp/workdir && cp -rf "$WORKDIR"/. /tmp/workdir/ && cd /tmp/workdir
 
 git config --global --add safe.directory /tmp/workdir
 git config --global --add advice.detachedHead false
 
 if [[ ! -z "$HEADREF" ]]
 then
-    git diff --no-color $HEADREF -- /tmp/workdir/.busted /tmp/workdir/src/HeadlessWrapper.lua /tmp/workdir/spec/ > /tmp/HeadPatch &&
-    git reset --hard $HEADREF && git clean -fd && git apply --allow-empty /tmp/HeadPatch
+    git diff --no-color "$HEADREF" -- /tmp/workdir/.busted /tmp/workdir/src/HeadlessWrapper.lua /tmp/workdir/spec/ > /tmp/HeadPatch &&
+    git reset --hard "$HEADREF" && git clean -fd && git apply --allow-empty /tmp/HeadPatch
 fi
 
 headsha=$(git rev-parse HEAD)
-devsha=$(git rev-parse $DEVREF)
+devsha=$(git rev-parse "$DEVREF")
 
 rm -rf /tmp/headsha && mkdir /tmp/headsha
 rm /tmp/workdir/src/Settings.xml
@@ -30,16 +30,16 @@ BUILDCACHEPREFIX='/tmp/headsha' busted --lua=luajit -r generate && date > "/tmp/
 
 if [[ ! -f "$CACHEDIR/$devsha" ]] # Output of builds outdated or nonexistent
 then
-	rm -rf $CACHEDIR/*.build
+	rm -rf "$CACHEDIR"/*.build
 
     # Keep new changes to tests related files
-    git diff --no-color $DEVREF -- /tmp/workdir/.busted /tmp/workdir/src/HeadlessWrapper.lua /tmp/workdir/spec/ > /tmp/DevPatch && \
-    git reset --hard $DEVREF && git clean -fd && git apply --allow-empty /tmp/DevPatch && \
+    git diff --no-color "$DEVREF" -- /tmp/workdir/.busted /tmp/workdir/src/HeadlessWrapper.lua /tmp/workdir/spec/ > /tmp/DevPatch && \
+    git reset --hard "$DEVREF" && git clean -fd && git apply --allow-empty /tmp/DevPatch && \
     cat /tmp/workdir/spec/builds.txt | dos2unix | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS="$LINKSBATCH" BUILDCACHEPREFIX="$CACHEDIR" busted --lua=luajit -r generate' && \
     BUILDCACHEPREFIX="$CACHEDIR" busted --lua=luajit -r generate && date > "$CACHEDIR/$devsha" && echo "[+] Build cache computed for $devsha (devsha)" || exit $?
 fi
 
-for build in $CACHEDIR/*.build
+for build in "$CACHEDIR"/*.build
 do
     BASENAME=$(basename "$build")
 
